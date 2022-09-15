a265c79d975a3ace90f639f5cee1c197c314e6c0
Tso4la1 <tsolajohn@gmail.com>
2022-09-15 14:28:01 +0100

    Source Code upload

--- src/carouselComponent.js
+++ src/carouselComponent.js
@@ -1,5 +1,7 @@
 import Carousel from "react-bootstrap/Carousel";
-import slider1 from './images/long.jpg'
+import slider1 from './images/long.jpg';
+import slider2 from './images/js.jpg';
+import slider3 from './images/view.jpg';
 
 const CarouselComponent = () => {
     return ( 
@@ -11,31 +13,31 @@ const CarouselComponent = () => {
             alt="First slide"
           />
           <Carousel.Caption>
-            <h3 className="sliderText">First slide label</h3>
+            <h3 className="sliderText"></h3>
             <p className="sliderText">If you're naturally yourself, you're beautiful. It's nice to just embrace the natural beauty within you.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item className="sliderHeight">
           <img
             className="d-block w-100"
-            src={slider1}
+            src={slider2}
             alt="Second slide"
           />
   
           <Carousel.Caption>
-            <h3 className="sliderText">Second slide label</h3>
+            <h3 className="sliderText"></h3>
             <p className="sliderText">Being natural is incredibly empowering for women because it's just who you are. </p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item className="sliderHeight">
           <img
             className="d-block w-100"
-            src={slider1}
+            src={slider3}
             alt="Third slide"
           />
   
           <Carousel.Caption>
-            <h3 className="sliderText">Third slide label</h3>
+            <h3 className="sliderText"></h3>
             <p className="sliderText">
             You're embracing all the beautiful things about you from your head to your toes.
             </p>
